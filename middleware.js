fef953c18357694f210176098f299aaea76eecb1
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-11 18:30:45 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,15 @@
+const requestLogger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().toISOString();
+
+  console.log(
+    `[${time}] ${method} ${url}`
+  );
+
+  next();
+};
+
+module.exports = {
+  requestLogger
+};
