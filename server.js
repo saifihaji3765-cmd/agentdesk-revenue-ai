e270581fc81e645fd372cc1d8cea98df4c95e81d
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-11 18:13:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 
+const routes = require("./routes");
+
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
     app: "AgentDesk Revenue AI",
-    message: "Server Running Successfully"
+    status: "Live"
   });
 });
 
