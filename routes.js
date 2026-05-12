7eb9fbcbf6bf74e2f420ad7e7d2a3c1a5c119a19
saifihaji3765-cmd <saifihaji3765@gmail.com>
2026-05-12 10:28:16 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,12 +6,24 @@ const {
   generateProposal
 } = require("./controllers");
 
+const {
+  saveTraining
+} = require("./trainingController");
+
 const router = express.Router();
 
 router.get("/api", getApiStatus);
 
 router.get("/api/health", getHealthStatus);
 
-router.post("/api/proposal", generateProposal);
+router.post(
+  "/api/proposal",
+  generateProposal
+);
+
+router.post(
+  "/api/training",
+  saveTraining
+);
 
 module.exports = router;
